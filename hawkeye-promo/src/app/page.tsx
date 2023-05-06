d3ad578f291ca88a4d3d11a9e54911f73033beaa
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2023-05-06 04:12:59 -0300

    Set-up for next/font % tailwind.config

---
+++ hawkeye-promo/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-white-1 font-calsans text-2xl'>Hola</h1>
+      <p className='text-grey-1 font-jakarta text-lg'>Chau</p>
+    </main>
+  )
+}
---
+++ hawkeye-promo/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      //Blues
+      "blue-1": "#090E1C",
+      "blue-2": "#131826",
+      "blue-3": "#171C2C",
+      "blue-4": "#1E263A",
+      //Greys
+      "grey-1": "#707FA4",
+      "grey-2": "#A0AED1",
+      "grey-3": "#C4CCE0",
+      //Whites
+      "white-1": "#E0E5F3",
+      //Brand
+      "brand-1": "#4ECB71",
+    },
+    fontSize: {
+      "sm": ["14px"],
+      "base": ["16px"],
+      "lg": ["20px"],
+      "xl": ["48px"],
+      "2xl": ["64px"],
+    },
+    fontFamily: {
+      calsans: ["var(--font-calsans)"],
+      jakarta: ["var(--font-jakarta)"],
+    },
+  },
+  plugins: [],
+};
