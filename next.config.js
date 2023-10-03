6c0c13a7838c2ce15a607d454d9e4170be8e17ad
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2023-10-02 22:00:36 -0300

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://hawkeye.com.ar/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
