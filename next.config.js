234ba4625c0c0027e6d3e0a5ba4c9f7c96f7fcbf
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2023-10-02 22:02:43 -0300

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://hawkeye.com.ar/:path*",
-      },
-    ];
-  },
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+  ],
 };
 
 module.exports = nextConfig;
